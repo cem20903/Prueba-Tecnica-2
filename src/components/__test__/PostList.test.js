3bfba7e1768417f75289cf2259b83d980e742139
cem20903 <cem20903@gmail.com>
2024-04-10 21:13:02 +0200

    Setup test with TS

---
+++ src/components/__test__/PostList.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/vue"
+import { createRouter, createWebHistory } from "vue-router"
+import { routes } from "../../router"
+import App from "../../App.vue"
+
+import store from "../../stores/user"
+
+const fetchMock = jest.fn()
+global.fetch = fetchMock
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+})
+
+test("routing", async () => {
+  router.push("/")
+  await router.isReady()
+
+  const mockFetch = Promise.resolve({
+    json: () => Promise.resolve(mockResponse),
+  })
+
+  fetchMock.mockImplementation(() => mockFetch)
+
+  const wrapper = render(App, {
+    global: {
+      plugins: [router],
+    },
+  })
+
+  screen.debug()
+  expect(wrapper.html()).toContain("Welcome to the blogging app")
+})
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	test: {
+    // enable jest-like global test APIs
+    globals: true,
+    // simulate DOM with happy-dom
+    // (requires installing happy-dom as a peer dependency)
+    environment: 'happy-dom'
+  },
 	plugins: [vue()],
 	resolve: {
 		alias: [
@@ -13,4 +20,5 @@ export default defineConfig({
 			},
 		],
 	},
+
 });
