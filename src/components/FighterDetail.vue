c4c5a9fbed90bd13f10b6af8c515ac7cee359027
cem20903 <cem20903@gmail.com>
2024-04-11 10:17:42 +0200

    Add computed kilos

--- src/components/FighterDetail.vue
+++ src/components/FighterDetail.vue
@@ -1,14 +1,26 @@
 <script setup>
-import { onMounted, ref } from "vue"
+import { onMounted, ref, computed } from "vue"
 import { useRoute } from "vue-router"
 import { getFighterById } from "@/services/fight"
-const router = useRoute()
 
+const POUND_TO_KILOGRAM = 0.453592
+
+const router = useRoute()
 const fighter = ref({})
 
 onMounted(async () => {
   const response = await getFighterById(router.params.id)
   fighter.value = response
+})
+
+const weightInKG = computed(() => {
+  if (!fighter.value.weight) {
+    return ""
+  }
+
+  const weightInPounds = fighter.value.weight.split(" ")[0]
+
+  return Math.round(weightInPounds * POUND_TO_KILOGRAM * 100) / 100
 })
 </script>
 
@@ -21,6 +33,7 @@ onMounted(async () => {
     <div>Altura: {{ fighter.height }}</div>
     <div>Apodo: {{ fighter.nickname }}</div>
     <div>Peso: {{ fighter.weight }}</div>
+    <div>Peso:: {{ weightInKG }} kg</div>
   </div>
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <nav class="menu"></nav>
+</template>
+
+<style scoped></style>
